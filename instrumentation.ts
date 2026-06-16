2a532281442e8a0326202592b25c96df0d138353
brunoagreis <126572415+brunoagreis@users.noreply.github.com>
2026-06-16 08:03:51 -0400

    Garante inicio do agendador CORE

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { startCoreAutomaticScheduler } = await import("./lib/judicial-core-automatic-scheduler")
-    startCoreAutomaticScheduler()
-  }
+  if (process.env.NEXT_RUNTIME === "edge") return
+
+  const { startCoreAutomaticScheduler } = await import("./lib/judicial-core-automatic-scheduler")
+  startCoreAutomaticScheduler()
 }
