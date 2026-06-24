4878cb66bad833dd4bd407e2fcf17ac8200b7657
brunoagreis <126572415+brunoagreis@users.noreply.github.com>
2026-06-24 10:45:38 -0400

    Cria rota padronizada de SIGTAP judicial

---
+++ app/(admin)/admin/judicial/sigtap/page.tsx
@@ -0,0 +1,5 @@
+import { JudicialSigTapAdminPage } from "@/components/modules/judicial-sigtap-admin-page"
+
+export default function SigtapPage() {
+  return <JudicialSigTapAdminPage initialTab="sigtap" />
+}
